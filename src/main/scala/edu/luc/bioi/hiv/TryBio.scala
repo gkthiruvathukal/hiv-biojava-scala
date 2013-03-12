39025d3947f98e5fdd5ed5a01559f94fc3b059a6
George K. Thiruvathukal <gkt@cs.luc.edu>
2013-03-11 21:03:25 -0500

    minor cosmetic changes to code

--- src/main/scala/edu/luc/bioi/hiv/TryBio.scala
+++ src/main/scala/edu/luc/bioi/hiv/TryBio.scala
@@ -2,6 +2,7 @@ package edu.luc.bioi.hiv
 
 import java.io.BufferedReader
 import java.io.FileReader
+
 import org.biojava.bio.seq.Sequence
 import org.biojava.bio.seq.io.SeqIOTools
 
@@ -17,8 +18,7 @@ object TryBio {
       null
   }
 
-  /*
-  def writeSomeFasta(): Unit = {
+/*  def writeSomeFasta(): Unit = {
     import org.biojavax.bio.seq._
     val sdb = new HashSequenceDB()
     val dna1 = DNATools.createDNASequence("agct", "Dna-12");
@@ -26,9 +26,7 @@ object TryBio {
     val dna2 = DNATools.createDNASequence("agagct", "Dna-13");
     sdb.addSequence(dna2)
     RichSequence.IOTools.writeFasta(System.out, sdb.sequenceIterator(), null);
-  }
-  *
-  */
+  }*/
 
   def getSourceInformation(sequence: Sequence): SourceInformation = {
     var featureIterator = sequence.features()
@@ -76,9 +74,9 @@ object TryBio {
       val seq = sequences.nextSequence()
       val seqInfo = getSequenceInformation(seq)
       if (seqInfo != null) {
-         val sourceInfo = getSourceInformation(seq)
-            if (sourceInfo != null)
-              processCDS(seq, seqInfo, sourceInfo)
+        val sourceInfo = getSourceInformation(seq)
+        if (sourceInfo != null)
+          processCDS(seq, seqInfo, sourceInfo)
       }
     }
   }
