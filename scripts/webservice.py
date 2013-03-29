fe5e05141a6732e617adf4a0f91a5b6fda16d472
George K. Thiruvathukal <gkt@cs.luc.edu>
2013-03-28 22:03:58 -0500

    proof of concept web service to browse the GenBank collection by gene (not yet fully discoverable with clickable resources but will finish shortly)

---
+++ scripts/webservice.py
@@ -0,0 +1,68 @@
+#
+# Q&D web service that gets precomputed HIV data from our MongoDB 
+#
+
+from flask import *
+from pymongo import *
+import json
+import StringIO
+
+FASTATEMPLATE=""">%(accession)s|%(gene)s|%(date)s|%(note)s
+%(sequence)s"""
+
+app = Flask(__name__)
+
+def request_wants_json():
+    best = request.accept_mimetypes \
+        .best_match(['application/json', 'text/html'])
+    return best == 'application/json' and \
+        request.accept_mimetypes[best] > \
+        request.accept_mimetypes['text/html']
+
+@app.route("/data")
+def get_databases():
+  client = MongoClient()
+  db_names = client.database_names()
+  text = '\n'.join(db_names)
+  print("DB Names",text)
+  resp = Response(text, status=200, mimetype='text/plain')
+  resp.headers['Link'] = 'http://localhost'
+  return resp
+
+def get_collection_gene_names_mongo(collection):
+   client = MongoClient()
+   db = client[collection]
+   cursor = db.posts.find()
+   gene_set = set()
+   for item in cursor:
+      gene_set.add(item['gene'])
+   return list(gene_set)
+
+def get_fasta(collection, gene):
+   client = MongoClient()
+   db = client[collection]
+   cursor = db.posts.find({ 'gene' : gene })
+   fasta = StringIO.StringIO()
+   for item in cursor:
+      fasta.write(FASTATEMPLATE % item)
+   text = fasta.getvalue()
+   fasta.close()
+   return text
+
+@app.route("/data/<collection>")
+def get_collection_gene_names(collection):
+   text = '\n'.join( get_collection_gene_names_mongo(collection))
+   resp = Response(text, status=200, mimetype='text/plain')
+   resp.headers['Link'] = 'http://localhost'
+   return resp
+  
+
+@app.route("/data/<collection>/<gene>")
+def get_collection_gene(collection, gene):
+   resp = Response(get_fasta(collection, gene), status=200, mimetype='text/plain')
+   resp.headers['Link'] = 'http://localhost'
+   return resp
+
+if __name__ == "__main__":
+    app.run()
+
